b7a005085b9a1c197745bf8304bef2aba41c2c74
VPapic <vjekopapic55@gmail.com>
2024-04-22 13:34:47 +0200

    zadnje promjene

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
 // src/index.js
 import express, { Express } from 'express'
+import 'reflect-metadata'
+import dataSource from './app-data-source'
 import homeRoutes from './routes/homeRoutes'
 import productRoutes from './routes/productRoutes'
 import cartRoutes from './routes/cartRoutes'
+import errorHandler from './middlewares/ErrorHandler'
+
+dataSource
+  .initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!')
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization:', err)
+  })
 
 const app: Express = express()
 const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(errorHandler)
 
 app.use('/', homeRoutes)
 // app.use('/users', userRoutes)
